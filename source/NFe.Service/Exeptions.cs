47b10e5b05f9b86acf7fb6c1708ed297022aa8cd
Suttiago <150452828+Suttiago@users.noreply.github.com>
2025-11-05 16:17:27 -0300

    refactor: limpando métodos não utilizados no UniNFe. ID #178093

--- source/NFe.Service/Exeptions.cs
+++ source/NFe.Service/Exeptions.cs
@@ -22,16 +22,6 @@ namespace NFe.Exceptions
             this.ErrorCode = Erro;
         }
 
-        /// <summary>
-        /// Construtor que já define uma mensagem pré-definida de exceção com possibilidade de complemento da mensagem
-        /// </summary>
-        /// <param name="CodigoErro">Código da mensagem de erro (Classe MsgErro)</param>
-        /// <param name="ComplementoMensagem">Complemento da mensagem de exceção</param>
-        public ExceptionSemInternet(ErroPadrao Erro, string ComplementoMensagem)
-            : base(MsgErro.ErroPreDefinido(Erro, ComplementoMensagem))
-        {
-            this.ErrorCode = Erro;
-        }
     }
 
     /// <summary>
@@ -41,33 +31,5 @@ namespace NFe.Exceptions
     {
         public ErroPadrao ErrorCode { get; private set; }
 
-        /// <summary>
-        /// Construtor que já define uma mensagem pré-definida de exceção
-        /// </summary>
-        /// <param name="CodigoErro">Código da mensagem de erro (Classe MsgErro)</param>
-        /// <remarks>
-        /// Autor: Wandrey Mundin Ferreira
-        /// Data: 16/03/2010
-        /// </remarks>
-        public ExceptionEnvioXML(ErroPadrao Erro)
-            : base(MsgErro.ErroPreDefinido(Erro))
-        {
-            this.ErrorCode = Erro;
-        }
-
-        /// <summary>
-        /// Construtor que ´já define uma mensagem pré-definida de exceção com possibilidade de complemento da mensagem
-        /// </summary>
-        /// <param name="CodigoErro">Código da mensagem de erro (Classe MsgErro)</param>
-        /// <param name="ComplementoMensagem">Complemento da mensagem de exceção</param>
-        /// <remarks>
-        /// Autor: Wandrey Mundin Ferreira
-        /// Data: 16/03/2010
-        /// </remarks>
-        public ExceptionEnvioXML(ErroPadrao Erro, string ComplementoMensagem)
-            : base(MsgErro.ErroPreDefinido(Erro, ComplementoMensagem))
-        {
-            this.ErrorCode = Erro;
-        }
     }
 }
