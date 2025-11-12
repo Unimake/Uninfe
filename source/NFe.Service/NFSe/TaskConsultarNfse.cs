68e912c20360c1121f1e5384af0a272e240b0a6b
Eduardo Cavazin <eduardotorcav@gmail.com>
2025-11-12 10:22:42 -0300

    refactor: Remove código de município de Osório - RS de padrão IPM da condicional, pois atualizou a versão para 1.20. ID #178418

--- source/NFe.Service/NFSe/TaskConsultarNfse.cs
+++ source/NFe.Service/NFSe/TaskConsultarNfse.cs
@@ -506,7 +506,7 @@ namespace NFe.Service.NFSe
                         versaoXML = "1.20";
 
                         if (codMunicipio == 4309308 || codMunicipio == 4316006 || codMunicipio == 4314050 ||
-                            codMunicipio == 4320206 || codMunicipio == 4313508 || codMunicipio == 4215802)
+                            codMunicipio == 4320206 || codMunicipio == 4215802)
                         {
                             versaoXML = "1.00";
                         }
