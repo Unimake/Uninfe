a730d9044f1dfcd542393f36ed7cfc7b442a8d7c
Gustavo Randi <gustarandiiii@gmail.com>
2026-01-29 09:37:45 -0300

    feat: Adição da versão 1.01 na task CancelarNFSe padrão PRONIM. ID #180472

--- source/NFe.Service/NFSe/TaskCancelarNfse.cs
+++ source/NFe.Service/NFSe/TaskCancelarNfse.cs
@@ -574,6 +574,12 @@ namespace NFe.Service.NFSe
                     {
                         versaoXML = "1.00";
                     }
+
+                    if (xmlDoc.OuterXml.Contains("versao=\"1.01\""))
+                    {
+                        versaoXML = "1.01";
+                    }
+
                     break;
 
                 case PadraoNFSe.SMARAPD:
