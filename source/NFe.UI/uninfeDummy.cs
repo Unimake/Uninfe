3b70b96585f9daea5f901bf895b2dd46eeeb97e7
Suttiago <150452828+Suttiago@users.noreply.github.com>
2026-05-07 10:28:33 -0300

    feat: adicionando DCe na UI da seleção de DFe. ID #183524

--- source/NFe.UI/uninfeDummy.cs
+++ source/NFe.UI/uninfeDummy.cs
@@ -115,6 +115,8 @@ namespace NFe.UI
             list.Add(new KeyValuePair<int, string>((int)TipoAplicativo.NFCe, EnumHelper.GetDescription(TipoAplicativo.NFCe)));
             list.Add(new KeyValuePair<int, string>((int)TipoAplicativo.NF3e, EnumHelper.GetDescription(TipoAplicativo.NF3e)));
             list.Add(new KeyValuePair<int, string>((int)TipoAplicativo.NFCom, EnumHelper.GetDescription(TipoAplicativo.NFCom)));
+            list.Add(new KeyValuePair<int, string>((int)TipoAplicativo.DCe, EnumHelper.GetDescription(TipoAplicativo.DCe)));
+
 
             if (!soConsulta)
             {
