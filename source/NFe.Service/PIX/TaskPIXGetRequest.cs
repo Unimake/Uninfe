796ee5ffe5725871167b9ac0ae5f2e358a1a0393
Eduardo Cavazin <eduardotorcav@gmail.com>
2026-06-01 17:13:57 -0300

    refactor: Atualiza serviços de PIX para utilizar comunicação via DLL. ID #183934

--- source/NFe.Service/PIX/TaskPIXGetRequest.cs
+++ source/NFe.Service/PIX/TaskPIXGetRequest.cs
@@ -1,28 +1,15 @@
-﻿using EBank.Solutions.Primitives.Billet.Models;
-using EBank.Solutions.Primitives.Enumerations;
-using EBank.Solutions.Primitives.PIX.Models.Consulta;
-using EBank.Solutions.Primitives.PIX.Request.Consulta;
 using NFe.Components;
 using NFe.Settings;
-using NFe.Validate;
 using System;
-using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Net;
 using System.Text;
 using System.Xml;
-using Unimake.AuthServer.Authentication;
-using Unimake.AuthServer.Security.Scope;
-using Unimake.EBank.Solutions.Services.PIX;
-using Unimake.Primitives.UDebug;
+using Unimake.Business.DFe.Servicos;
 
 namespace NFe.Service
 {
     public class TaskPIXGetRequest : TaskAbst
     {
-        public static DebugScope<DebugStateObject> debugScope;
-
         public TaskPIXGetRequest(string arquivo)
         {
             Servico = Servicos.PIXGetRequest;
@@ -31,181 +18,101 @@ namespace NFe.Service
             ConteudoXML.Load(arquivo);
         }
 
-        public override async void Execute()
+        public override void Execute()
         {
             var emp = Empresas.FindEmpresaByThread();
-
-            var extEnvio = Propriedade.Extensao((Servico == Servicos.PIXGetRequest ? Propriedade.TipoEnvio.PIXGetRequest : Propriedade.TipoEnvio.PIXConsultaRequest)).EnvioXML;
-            var extRetorno = Propriedade.Extensao((Servico == Servicos.PIXGetRequest ? Propriedade.TipoEnvio.PIXGetRequest : Propriedade.TipoEnvio.PIXConsultaRequest)).RetornoXML;
+            var extEnvio = Propriedade.Extensao(TipoEnvioPIX).EnvioXML;
+            var extRetorno = Propriedade.Extensao(TipoEnvioPIX).RetornoXML;
+            var file = Functions.ExtrairNomeArq(NomeArquivoXML, extEnvio) + extRetorno;
+            var pathXml = Path.Combine(Empresas.Configuracoes[emp].PastaXmlRetorno, file);
 
             try
             {
-                #region Validar o XML
-
-                var validarXML = new ValidarXML
+                if (string.IsNullOrWhiteSpace(Empresas.Configuracoes[emp].AppID) || string.IsNullOrWhiteSpace(Empresas.Configuracoes[emp].Secret))
                 {
-                    TipoArqXml = new TipoArquivoXML
-                    {
-                        cArquivoSchema = Path.Combine(Propriedade.PastaExecutavel, @"NFe\schemas\PIX\" + (Servico == Servicos.PIXGetRequest ? "PIXGetRequest_1_00.xsd" : "PIXConsultaRequest_1_00.xsd")),
-                        nRetornoTipoArq = 1
-                    }
-                };
-
-                validarXML.ValidarArqXML(ConteudoXML, NomeArquivoXML);
-                if (validarXML.Retorno != 0)
-                {
-                    throw new Exception(validarXML.RetornoString.Replace("\r\n", ""));
+                    throw new Exception("Para utilizar o serviço de PIX é necessário configurar no UniNFe o AppID e Secret do eBank.");
                 }
 
-                #endregion
-
-                #region Validar TAGS do XML
-
-                var beneficiario = ConteudoXML.GetElementsByTagName("Beneficiario");
-
-                if (beneficiario.Count <= 0)
-                {
-                    throw new Exception("É obrigatório informar o grupo de tag <Beneficiario> no XML.");
-                }
-
-                var conta = ((XmlElement)beneficiario[0]).GetElementsByTagName("Conta");
-
-                if (conta.Count <= 0)
-                {
-                    throw new Exception("É obrigatório informar o grupo de tag <Conta>, que fica dentro do grupo de tag <Beneficiario>, no XML.");
-                }
-                if (((XmlElement)conta[0]).GetElementsByTagName("Agencia").Count <= 0)
-                {
-                    throw new Exception("É obrigatório informar a tag <Agencia>, que fica dentro do grupo de tag <Conta>, no XML.");
-                }
-                if (((XmlElement)conta[0]).GetElementsByTagName("Numero").Count <= 0)
-                {
-                    throw new Exception("É obrigatório informar a tag <Numero>, que fica dentro do grupo de tag <Conta>, no XML.");
-                }
-                if (((XmlElement)conta[0]).GetElementsByTagName("Banco").Count <= 0)
-                {
-                    throw new Exception("É obrigatório informar a tag <Banco>, que fica dentro do grupo de tag <Conta>, no XML.");
-                }
-
-                if (((XmlElement)beneficiario[0]).GetElementsByTagName("Inscricao").Count <= 0)
-                {
-                    throw new Exception("É obrigatório informar a tag <Inscricao>, que fica dentro do grupo de tag <Beneficiario>, no XML.");
-                }
-                if (((XmlElement)beneficiario[0]).GetElementsByTagName("Nome").Count <= 0)
-                {
-                    throw new Exception("É obrigatório informar a tag <Nome>, que fica dentro do grupo de tag <Beneficiario>, no XML.");
-                }
-
-                #endregion
-
-                #region Consultar
-
-                var pixGetRequest = new PIXGetRequest
-                {
-                    ConfigurationId = TFunctions.GetXmlValue(ConteudoXML, "ConfigurationId"),
-                    StartDate = DateTime.ParseExact(ConteudoXML.GetElementsByTagName("StartDate")[0].InnerText, "yyyy-MM-dd", CultureInfo.InvariantCulture),
-                    EndDate = DateTime.ParseExact(ConteudoXML.GetElementsByTagName("EndDate")[0].InnerText, "yyyy-MM-dd", CultureInfo.InvariantCulture),
-                    TxId = (ConteudoXML.GetElementsByTagName("TxId").Count > 0 ? ConteudoXML.GetElementsByTagName("TxId")[0].InnerText.Trim() : null),
-                    Testing = Convert.ToBoolean(ConteudoXML.GetElementsByTagName("Testing")[0].InnerText),
-                    Beneficiario = new Beneficiario
-                    {
-                        Conta = new ContaCorrente
-                        {
-                            Agencia = ((XmlElement)conta[0]).GetElementsByTagName("Agencia")[0].InnerText,
-                            Numero = ((XmlElement)conta[0]).GetElementsByTagName("Numero")[0].InnerText,
-                            Banco = (Banco)Convert.ToInt32(((XmlElement)conta[0]).GetElementsByTagName("Banco")[0].InnerText)
-                        },
-                        Inscricao = ((XmlElement)beneficiario[0]).GetElementsByTagName("Inscricao")[0].InnerText,
-                        Nome = ((XmlElement)beneficiario[0]).GetElementsByTagName("Nome")[0].InnerText
-                    }
-                };
-
-                //Verificar se o intervalo entre a data inicial e final não supera 5 dias, se superar tem que rejeitar pq no eBank só aceita no máximo 5 dias, assim evitamos o consumo da API.
-                if (((TimeSpan)(pixGetRequest.EndDate - pixGetRequest.StartDate)).Days >= 5)
-                {
-                    throw new Exception("O período de consulta para o PIX, entre as datas \"StartDate\" e \"EndDate\", não pode exceder 5 dias.");
-                }
-
-                var testing = AuthApiScopeHelper.GetTesting(ConteudoXML.DocumentElement);
-                var useHomologServer = AuthApiScopeHelper.ResolveUseHomologServer(testing, ConteudoXML.DocumentElement);
-                debugScope = AuthApiScopeHelper.CreateDebugScopeIfNeeded(useHomologServer, "https://ebank.sandbox.unimake.software/api/v1/");
-
-                var authenticatedScope = AuthApiScopeHelper.CreateAuthenticatedScopeEBank(emp);
-
-                var respostas = new List<PIXItem>();
-
-                var pagina = 1;
-                while (true)
-                {
-                    pixGetRequest.PageNumber = pagina;
-
-                    var pixService = new PIXService();
-                    var response = await pixService.GetAsync(pixGetRequest, authenticatedScope);
-
-                    respostas.AddRange(response.Items);
-
-                    if (pagina >= response.PageInfo.TotalPages)
-                    {
-                        break;
-                    }
-
-                    ++pagina;
-                }
-
-                authenticatedScope.Dispose();
-
-                #endregion
-
-                #region Gravar XML de Retorno
-
-                var file = Functions.ExtrairNomeArq(NomeArquivoXML, extEnvio) + extRetorno;
-                var pathXml = Path.Combine(Empresas.Configuracoes[emp].PastaXmlRetorno, file);
-
-                GerarXmlRetorno(pathXml, respostas);
-
-                #endregion
-            }
-            catch (Unimake.EBank.Solutions.Exceptions.ResponseException ex)
-            {
-                var file = Functions.ExtrairNomeArq(NomeArquivoXML, extEnvio) + extRetorno;
-                var pathXml = Path.Combine(Empresas.Configuracoes[emp].PastaXmlRetorno, file);
-                var traceId = ApiExceptionHelper.ExtrairTraceId(ex.GetLastException());
-
-                if (ex.StatusCode == HttpStatusCode.NotFound)
-                {
-                    GerarXmlRetorno(pathXml, null, "Nenhum Movimento PIX foi localizado.", "2", traceId);
-                }
-                else
-                {
-                    GerarXmlRetorno(pathXml, null, ex.GetLastException().Message.Replace("\r\n", ""), "", traceId);
-                }
+                ExecuteDLL(emp);
             }
             catch (Exception ex)
             {
-                var file = Functions.ExtrairNomeArq(NomeArquivoXML, extEnvio) + extRetorno;
-                var pathXml = Path.Combine(Empresas.Configuracoes[emp].PastaXmlRetorno, file);
-                var traceId = ApiExceptionHelper.ExtrairTraceId(ex.GetLastException());
-
-                GerarXmlRetorno(pathXml, null, ex.GetLastException().Message.Replace("\r\n", ""), "", traceId);
+                var lastException = ex.GetLastException();
+                var traceId = ApiExceptionHelper.ExtrairTraceId(lastException);
+                GerarXmlRetornoErro(pathXml, lastException.Message.Replace("\r\n", ""), traceId);
             }
             finally
             {
                 try
                 {
-                    //Deletar o arquivo de solicitação do serviço
                     Functions.DeletarArquivo(NomeArquivoXML);
                 }
                 catch
                 {
-                    //Se falhou algo na hora de deletar o XML de solicitação do serviço,
-                    //infelizmente não posso fazer mais nada, o UniNFe vai tentar mandar
-                    //o arquivo novamente para o webservice
-                    //Wandrey 09/03/2010
+                    // Se falhar a exclusao, o UniNFe tentara processar o arquivo novamente.
                 }
             }
         }
 
-        private void GerarXmlRetorno(string path, List<PIXItem> respostas, string exceptionMessage = "", string status = "", string traceId = "")
+        #region ExecuteDLL
+
+        private void ExecuteDLL(int emp)
+        {
+            var finalArqEnvio = Propriedade.Extensao(TipoEnvioPIX).EnvioXML;
+            var finalArqRetorno = Propriedade.Extensao(TipoEnvioPIX).RetornoXML;
+
+            var configuracao = new Configuracao
+            {
+                PrepararConexaoTLSAntesDoEnvio = Empresas.Configuracoes[emp].AtivarPreparacaoTLSAntesEnvioXML,
+                CertificadoDigital = Empresas.Configuracoes[emp].X509Certificado,
+                TipoAmbiente = (Unimake.Business.DFe.Servicos.TipoAmbiente)Empresas.Configuracoes[emp].AmbienteCodigo,
+                CodigoUF = Empresas.Configuracoes[emp].UnidadeFederativaCodigo,
+                AppId = Empresas.Configuracoes[emp].AppID,
+                Secret = Empresas.Configuracoes[emp].Secret
+            };
+
+            try
+            {
+                var assembly = typeof(Configuracao).Assembly;
+                var pixType = assembly.GetType($"Unimake.Business.DFe.Servicos.PIX.{NomeServicoPIX}");
+                if (pixType == null) throw new Exception($"A implementação do serviço PIX {NomeServicoPIX} não foi localizada na DLL.");
+
+                var pixInstance = Activator.CreateInstance(pixType, new object[] { ConteudoXML.OuterXml, configuracao });
+                var executarMethod = pixInstance.GetType().GetMethod("Executar");
+                if (executarMethod == null) throw new Exception($"A implementação do serviço PIX {NomeServicoPIX} não expõe o método Executar().");
+                executarMethod.Invoke(pixInstance, null);
+
+                var retornoProp = pixInstance.GetType().GetProperty("RetornoWSString");
+                if (retornoProp != null) vStrXmlRetorno = retornoProp.GetValue(pixInstance)?.ToString();
+
+                if (string.IsNullOrWhiteSpace(vStrXmlRetorno))
+                {
+                    throw new Exception($"A implementação do serviço PIX {NomeServicoPIX} não retornou RetornoWSString. Atualize a DLL para fornecer o XML de retorno pronto.");
+                }
+
+                XmlRetorno(finalArqEnvio, finalArqRetorno);
+
+                var disposeMethod = pixInstance.GetType().GetMethod("Dispose");
+                disposeMethod?.Invoke(pixInstance, null);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro ao executar DLL PIX {NomeServicoPIX}: {ex.Message}", ex);
+            }
+        }
+
+        #endregion ExecuteDLL
+
+        private Propriedade.TipoEnvio TipoEnvioPIX =>
+            Servico == Servicos.PIXGetRequest ? Propriedade.TipoEnvio.PIXGetRequest : Propriedade.TipoEnvio.PIXConsultaRequest;
+
+        private string NomeServicoPIX =>
+            Servico == Servicos.PIXGetRequest ? "PixConsultar" : "PixCobrancaConsultar";
+
+        private string NomeElementoRetorno =>
+            Servico == Servicos.PIXGetRequest ? "PIXGetResponse" : "PIXConsultaResponse";
+
+        private void GerarXmlRetornoErro(string path, string motivo, string traceId)
         {
             var oSettings = new XmlWriterSettings();
             var c = new UTF8Encoding(false);
@@ -215,96 +122,22 @@ namespace NFe.Service
             oSettings.IndentChars = " ";
             oSettings.NewLineOnAttributes = false;
             oSettings.OmitXmlDeclaration = false;
-            XmlWriter oXmlGravar = null;
 
-            try
+            using (var oXmlGravar = XmlWriter.Create(path, oSettings))
             {
-                var cultura = CultureInfo.CreateSpecificCulture("en-US");
-                cultura.NumberFormat.NumberDecimalSeparator = ".";
-
-                oXmlGravar = XmlWriter.Create(path, oSettings);
                 oXmlGravar.WriteStartDocument();
-                oXmlGravar.WriteStartElement((Servico == Servicos.PIXGetRequest ? "PIXGetResponse" : "PIXConsultaResponse"));
+                oXmlGravar.WriteStartElement(NomeElementoRetorno);
+                oXmlGravar.WriteElementString("Status", "999");
+                oXmlGravar.WriteElementString("Motivo", motivo);
 
-                if (respostas.Count == 0)
+                if (!string.IsNullOrWhiteSpace(traceId))
                 {
-                    if (!string.IsNullOrWhiteSpace(status))
-                    {
-                        oXmlGravar.WriteElementString("Status", status);
-                    }
-                    else
-                    {
-                        oXmlGravar.WriteElementString("Status", "999");
-                    }
-                    oXmlGravar.WriteElementString("Motivo", exceptionMessage);
-
-                    if (!string.IsNullOrWhiteSpace(traceId))
-                    {
-                        oXmlGravar.WriteElementString("TraceId", traceId);
-                    }
-                }
-                else
-                {
-                    oXmlGravar.WriteElementString("Status", (respostas.Count > 0 ? "1" : "2"));
-                    oXmlGravar.WriteElementString("Motivo", (respostas.Count == 1 ? "Movimento PIX foi localizado." : (respostas.Count == 0 ? "Nenhum Movimento PIX foi localizado." : "Movimentos PIX localizados.")));
-
-                    if (Servico == Servicos.PIXGetRequest)
-                    {
-                        if (respostas.Count == 1)
-                        {
-                            oXmlGravar.WriteElementString("TxId", respostas[0].TxId);
-                            oXmlGravar.WriteElementString("Valor", respostas[0].Valor.ToString("N2", cultura));
-                            oXmlGravar.WriteElementString("Horario", respostas[0].Horario.ToString("yyyy-MM-dd'T'HH:mm:ss"));
-
-                            oXmlGravar.WriteStartElement("Pagador");
-                            oXmlGravar.WriteElementString("Nome", respostas[0].Pagador.Nome);
-                            oXmlGravar.WriteElementString("Inscricao", respostas[0].Pagador.Inscricao);
-                            oXmlGravar.WriteEndElement(); //Pagador
-                        }
-                    }
-                    else
-                    {
-                        if (respostas.Count > 0)
-                        {
-                            oXmlGravar.WriteStartElement("Items");
-
-                            for (var item = 0; item < respostas.Count; item++)
-                            {
-                                oXmlGravar.WriteStartElement("Item");
-                                oXmlGravar.WriteAttributeString("Id", (item + 1).ToString());
-
-                                oXmlGravar.WriteElementString("TxId", respostas[item].TxId);
-                                oXmlGravar.WriteElementString("Valor", respostas[item].Valor.ToString("N2", cultura));
-                                oXmlGravar.WriteElementString("Horario", respostas[item].Horario.ToString("yyyy-MM-dd'T'HH:mm:ss"));
-
-                                oXmlGravar.WriteStartElement("Pagador");
-                                oXmlGravar.WriteElementString("Nome", respostas[item].Pagador.Nome);
-                                oXmlGravar.WriteElementString("Inscricao", respostas[item].Pagador.Inscricao);
-                                oXmlGravar.WriteEndElement(); //Pagador
-
-                                oXmlGravar.WriteEndElement(); //Item
-                            }
-
-                            oXmlGravar.WriteEndElement(); //Items
-                        }
-                    }
+                    oXmlGravar.WriteElementString("TraceId", traceId);
                 }
 
                 oXmlGravar.WriteElementString("UniNFeVersao", Propriedade.Versao + " | " + Propriedade.DataHoraUltimaModificacaoAplicacao.Replace("/", "-"));
-                oXmlGravar.WriteEndElement(); //PIXGetResponse ou PIXConsultaResponse
+                oXmlGravar.WriteEndElement();
                 oXmlGravar.WriteEndDocument();
-                oXmlGravar.Flush();
-                oXmlGravar.Close();
-            }
-            finally
-            {
-                if (oXmlGravar != null)
-                {
-                    if (oXmlGravar.WriteState != WriteState.Closed)
-                    {
-                        oXmlGravar.Close();
-                    }
-                }
             }
         }
     }
