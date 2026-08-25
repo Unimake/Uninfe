7bc3a639cd1feecc7913361385fe07126191189e
Eduardo Cavazin <eduardotorcav@gmail.com>
2026-08-25 11:19:59 -0300

    Resolução de Merge

--- source/NFe.Service/NFSe/TaskConsultarNfsePDF.cs
+++ source/NFe.Service/NFSe/TaskConsultarNfsePDF.cs
@@ -189,6 +189,7 @@ namespace NFe.Service.NFSe
 
             switch(padraoNFSe)
             {
+                case PadraoNFSe.NACIONAL:
                 case PadraoNFSe.PRODATA:
                     nomeTag = "Base64Pdf";
                     break;
